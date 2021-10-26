adab8ee3428c6a08b448b22d5ce416616abb1e15
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-26 17:49:09 +0300

    implement list item deletion on click

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,40 @@
-import TodoList from "../TodoList/TodoList";
+import {Component} from "react";
+import TodoList from "../TodoList";
 import AppHeader from "../AppHeader";
-import SearchPanel from "../SearchPanel/SearchPanel";
+import SearchPanel from "../SearchPanel";
+import ItemStatusFilter from "../ItemStatusFilter";
 import './App.css'
-import ItemStatusFilter from "../ItemStatusFilter/ItemStatusFilter";
 
-const App = () => {
-    const todoData = [
-        {id: 1, label: 'Learn React', important: true},
-        {id: 2, label: 'Master Redux', important: false},
-        {id: 3, label: 'Build App'},
-    ]
+class App extends Component {
+    state = {
+        todoData: [
+            {id: 1, label: 'Learn React', important: true},
+            {id: 2, label: 'Master Redux', important: false},
+            {id: 3, label: 'Build App'},
+        ]
+    }
 
-    return (
-        <div className="todo-app">
-            <AppHeader />
-            <div className="search-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    onDeleted = (id) => {
+        this.setState(({ todoData }) => {
+            return {todoData: todoData.filter((item) => item.id !== id)}
+        })
+    }
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader />
+                <div className="search-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={(id) => this.onDeleted(id)}
+                />
             </div>
-            <TodoList todos={todoData} onDeleted={(id) => console.log('delete item', id)} />
-        </div>
-    );
-};
+        );
+    }
+}
 
 export default App;
