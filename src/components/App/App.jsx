efcbba62925480ec7e194f5c8ac60f26c5663138
Dmytro Pavliuk <pavlyukdm@gmail.com>
2021-10-28 20:46:20 +0300

    remove MUI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,44 +7,49 @@ import ItemAddControl from "../ItemAddControl";
 import './App.css'
 
 class App extends Component {
-  state = {
-    todoData: [
-      {id: 1, label: 'Learn React', important: true},
-      {id: 2, label: 'Master Redux', important: false},
-      {id: 3, label: 'Build App'},
-    ]
-  }
+    state = {
+        todoData: [
+            {id: 1, label: 'Learn React', important: true},
+            {id: 2, label: 'Master Redux', important: false},
+            {id: 3, label: 'Build App', important: false},
+        ]
+    }
 
-  onDeleted = (id) => {
-    this.setState(({todoData}) => {
-      return {todoData: todoData.filter((item) => item.id !== id)}
-    })
-  }
+    onDeleted = (id) => {
+        this.setState(({todoData}) => {
+            return {todoData: todoData.filter((item) => item.id !== id)}
+        })
+    }
 
-  onItemAdded = (text) => {
-    this.setState(({todoData}) => {
-      return {todoData: [...todoData, {id: 4, label: text, important: false}]};
-    })
-    console.log('click')
-  }
+    onItemAdded = (text) => {
+        const newItem = {
+            id: new Date(),
+            label: text,
+            important: false
+        }
+        this.setState(({todoData}) => {
+            return {todoData: [...todoData, newItem]};
+        })
+        console.log('click')
+    }
 
-  render() {
-    return (
-      <div className="todo-app">
-        <AppHeader/>
-        <div className="search-panel d-flex">
-          <SearchPanel/>
-          <ItemStatusFilter/>
-        </div>
-        <TodoList
-          todos={this.state.todoData}
-          onDeleted={(id) => this.onDeleted(id)}
-        />
-        <ItemAddControl onItemAdded={this.onItemAdded}
-        />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <ItemStatusFilter/>
+                </div>
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={(id) => this.onDeleted(id)}
+                />
+                <ItemAddControl onItemAdded={this.onItemAdded}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/ItemStatusFilter/ItemStatusFilter.jsx
+++ src/components/ItemStatusFilter/ItemStatusFilter.jsx
@@ -1,14 +1,16 @@
 import {Component} from "react";
-import {Button, ButtonGroup} from "@mui/material";
 
 class ItemStatusFilter extends Component{
     render() {
         return (
-            <ButtonGroup variant="outlined">
-                <Button variant="contained">All</Button>
-                <Button>Active</Button>
-                <Button>Done</Button>
-            </ButtonGroup>
+          <div className="btn-group">
+            <button type="button"
+                    className="btn btn-info">All</button>
+            <button type="button"
+                    className="btn btn-outline-secondary">Active</button>
+            <button type="button"
+                    className="btn btn-outline-secondary">Done</button>
+          </div>
         )
     }
 }
